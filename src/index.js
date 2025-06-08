4426f1408d684d0b4d8e7b16a9ff3711f4e96fc5
totrungkiet26102003 <totrungkiet26102003@example.com>
2025-06-08 11:13:32 +0700

    Template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/favicon.ico", (req, res) => res.status(204));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+// To run this code, make sure you have Node.js and Express installed.
